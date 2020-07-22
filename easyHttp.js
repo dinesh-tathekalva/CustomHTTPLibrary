c5875c8c7f5c0c6af55a8abea62ff4f7c115196a
Mickey <mickey@Mickeys-MacBook-Pro.local>
2020-07-22 16:13:50 -0500

    callbacks updated

--- easyHttp.js
+++ easyHttp.js
@@ -3,13 +3,15 @@ function easyHTTP(){
 }
 
 //Make a HTTP GET Request
-easyHTTP.prototype.get = function(url){
+easyHTTP.prototype.get = function(url, callback){
     this.http.open('GET', url, true)
     
     let self = this
     this.http.onload = function(){
         if(self.http.status === 200){
-            return self.http.responseText
+            callback(null, self.http.responseText)
+        }else{
+            callback(`Error: ${self.http.status}`, null)
         }
     }
     this.http.send()
