ea6fc5bda9bd0a65f837c6fd3dd1127bec9cb3c5
Mickey <mickey@Mickeys-MacBook-Pro.local>
2020-07-22 21:09:50 -0500

    https methods updated

--- easyHttp.js
+++ easyHttp.js
@@ -18,10 +18,42 @@ easyHTTP.prototype.get = function(url, callback){
 }
 
 //Make a HTTP POST Request
+easyHTTP.prototype.post = function(url, data, callback){
+    this.http.open('POST', url, true)
+    this.http.setRequestHeader('content-type', 'application/json')
 
+    let self = this
+    this.http.onload = function(){
+        callback(null, self.http.responseText)
+    }
+
+    this.http.send(JSON.stringify(data))
+}
 
 //Make a HTTP PUT Request
 
+easyHTTP.prototype.post = function(url, data, callback){
+    this.http.open('PUT', url, true)
+    this.http.setRequestHeader('content-type', 'application/json')
 
+    let self = this
+    this.http.onload = function(){
+        callback(null, self.http.responseText)
+    }
+
+    this.http.send(JSON.stringify(data))
+}
 //Make a HTTP DELETE Request
- 
+easyHTTP.prototype.delete = function(url, callback){
+    this.http.open('DELETE', url, true)
+    
+    let self = this
+    this.http.onload = function(){
+        if(self.http.status === 200){
+            callback(null, 'Post Deleted')
+        }else{
+            callback(`Error: ${self.http.status}`, null)
+        }
+    }
+    this.http.send()
+}
